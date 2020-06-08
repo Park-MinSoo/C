de4c65933395de8fd09ff087213803db551fca7c
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-08 14:15:49 +0900

    Update Lab1.cs

--- Lab1.cs
+++ Lab1.cs
@@ -15,6 +15,13 @@ namespace OutStreamFormat
             double df = 3.45;
             Console.WriteLine("{0,10}{1}{2}{0}",i,s,df);
             // i,s,df가 index에 맞추어 {매개변수 순서,출력할 자릿수} 자리에 할당이 된다.
+
+            Console.WriteLine("{0:000000}", 12345); // 0의 갯수
+            Console.WriteLine("{0:D6}", 12345); // D 정수
+            Console.WriteLine("{0:.0}", 12345.6789);  // 0의 갯수
+            Console.WriteLine("{0:N2}", 12345.6789);  // N 정수
+            Console.WriteLine("{0:F3}", 12345.6789);  // F 정수
+
         }
     }
 }
