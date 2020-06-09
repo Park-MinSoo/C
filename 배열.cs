03d6733e933095c592edc95d5c9e6d748c582ecd
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-09 10:46:44 +0900

    add 배열

---
+++ 배열.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 배열
+{
+    class 배열
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[10];
+            for(int i =0;i<arr.Length;i++)
+            {
+                arr[i] = rand.Next(100);
+            }
+            Array.Sort(arr);
+
+            foreach(int elem in arr)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
