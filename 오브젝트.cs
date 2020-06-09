11929badfad97ed9caebe8b606d87a4dbd2d557c
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-09 09:44:54 +0900

    Create 오브젝트.cs

---
+++ 오브젝트.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutObject
+{
+    class 오브젝트
+    {
+        static void Main(string[] args)
+        {
+            object obj=8;
+            Console.WriteLine(obj);
+            obj = '3';
+            Console.WriteLine(obj);
+            obj = "가나다";
+            Console.WriteLine(obj);
+            obj = 3.4;
+            Console.WriteLine(obj);
+            // obj는 모든 것의 기반이기 때문에 다 가능하다.
+
+            var v = 8;
+            Console.WriteLine(v);
+            v = 'a';
+            Console.WriteLine(v);
+        }
+    }
+}
