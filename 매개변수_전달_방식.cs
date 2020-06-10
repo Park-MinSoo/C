d9c8ec6b1afc797911d85467afa37d084896ce1c
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-10 14:40:37 +0900

    Create 매개변수_전달_방식.cs

---
+++ 매개변수_전달_방식.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 매개변수_전달_방식
+{
+    class 매개변수_전달_방식
+    {
+        static void Main(string[] args)
+        {
+            int iq = 200;
+            Study(iq, 3);
+            Console.WriteLine("아이큐 : {0}", iq);
+        }
+
+        private static void Study(int iq, int v)
+        {
+            Console.WriteLine("{0}시간 공부", v);
+            iq += v;
+            Console.WriteLine("Study : {0}", iq);
+        }
+    }
+}
