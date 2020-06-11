8adadc037f40b08e19ac8d355fabc1785e95a99b
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-11 17:14:48 +0900

    Unit Program 추가

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 유닛응용
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyApplication myap = MyApplication.Singleton;
+            myap.Init();
+            myap.Run();
+            myap.Exit();
+        }
+    }
+}
