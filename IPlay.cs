ba61e4ead3ef6fc77eb4e015680234bb23f28316
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-12 11:51:52 +0900

    add 인터페이스 개요

---
+++ IPlay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 인터페이스_개요
+{
+    interface IPlay
+    {
+        void Play();
+    }
+
+    class Musician : IPlay
+    {
+     public void Play()
+        {
+            Console.WriteLine("연주하다.");
+        }
+    }
+    class SportsMan : IPlay
+    {
+        public void Play()
+        {
+            Console.WriteLine("운동하다.");
+        }
+    }
+}
---
+++ 인터페이스_개요.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 인터페이스_개요
+{
+    class 인터페이스_개요
+    {
+        static void Main(string[] args)
+        {
+            SportsMan sm = new SportsMan();
+            Musician mu = new Musician();
+            StartPlay(sm);
+            StartPlay(mu);
+        }
+        private static void StartPlay(IPlay iplay)
+        {
+            iplay.Play();
+        }
+    }
+}
