18a69f4db92fedae0885280f6d25193b22d24d34
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-15 11:38:30 +0900

    add 디자이너 응용

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 첫번째응용
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string data = tbox_name.Text;
+            tbox_name.Text = string.Empty;
+            lb_name.Text = data;
+        }
+    }
+}
