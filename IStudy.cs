9b905157013dc96ed052c6cddb10f207538aa9ea
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-15 10:03:08 +0900

    add 인터페이스

---
+++ IStudy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 인터페이스와_컬렉션
+{
+    interface IStudy
+    {
+        void Study();
+    }
+    class Student : IStudy
+    {
+        void IStudy.Study()
+        {
+            Console.WriteLine("공부하다");
+        }
+    }
+    class Student2 : IStudy
+    {
+        public void Study()
+        {
+            Console.WriteLine("공부하다");
+        }
+    }
+}
---
+++ 인터페이스.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 인터페이스와_컬렉션
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+            //stu.Study();
+            GoToLibrary(stu);
+            Student2 stu2 = new Student2();
+            stu2.Study();
+            GoToLibrary(stu);
+        }
+
+        private static void GoToLibrary(IStudy istudy)
+        {
+            istudy.Study();
+        }
+    }
+}
