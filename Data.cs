ae481d62dd393d56293d90d557c449ee25dfccb8
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-15 11:10:27 +0900

    add 사용자 정의 형식 보관

---
+++ Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 사용자_정의_형식_보관
+{
+    class Data:IComparable<Data>// IComparable은 분류기준에 대하여 어떻게 할지 설정해주는것이므로 오직 1가지만 존재한다.
+    {
+        public int Num { get; private set; }
+        public string Name { get; private set; }
+        public Data(int num, string name)
+        {
+            Num = num;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0},{1}",Num,Name);
+        }
+        public int CompareTo(Data other)
+        {
+           return Num - other.Num;
+           // return Name.CompareTo(other.Name);
+        }
+    }
+    public class DataCompapreByNum:IComparer<Data>  // IComparer는 여러개가 존재 할 수 있으며 다양한 분류 기준을 더 설정해 주는 것이다.
+    {
+        public int Compare(Data x, Data y)
+        {
+            return x.Num - y.Num;
+        }
+    }
+}
---
+++ 사용자_정의_형식_보관.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 사용자_정의_형식_보관
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Data> datas = new List<Data>();
+            datas.Add(new Data(3, "홍길동"));
+            datas.Add(new Data(4, "김길동"));
+            datas.Add(new Data(2, "박길동"));
+
+            datas.Add(new Data(7, "최길동"));
+            datas.Add(new Data(1, "주길동"));
+            datas.Sort();   // 이대로만 하면 이름순 정렬인지, 숫자순 정렬인지 모르기 때문에 정렬되지 않는다.
+            foreach (Data data in datas)
+
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("========");
+            datas.Sort(new DataCompapreByNum());
+            foreach (Data data in datas)
+            {
+                Console.WriteLine(data);
+            }
+
+
+        }
+    }
+}
