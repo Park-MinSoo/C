251ca043e3d4be59e5696b758a1f2fced7b10614
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-16 10:03:31 +0900

    update 대리자

---
+++ Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 대리자_소개
+{
+    class Manager
+    {
+        public event MyDele2 dele;
+        public int Sum { get; private set; }
+        public void Dolt(int a)
+        {
+            for(int i = 0;i<a;i++)
+            {
+                Sum += i;
+                if(Sum >=100)
+                {
+                    dele();
+                    Sum = 0;
+                }
+            }
+        }
+    }
+}
--- 대리자.cs
+++ 대리자.cs
@@ -13,12 +13,32 @@ namespace 대리자_소개
         {
             MyDelete dele = Add;
             int re = dele(3, 4);
-            Console.WriteLine("결과 : {0}",re);
+            Console.WriteLine("결과 : {0}", re);
 
-            MyDelete dele2 = Sub;
-            int re2 = dele2(3, 4);
+            MyDelete delee = Sub;
+            int re2 = delee(3, 4);
             Console.WriteLine("결과 : {0}", re2);
+
+            Manager man = new Manager();
+            man.dele += Man_dele;
+            Console.WriteLine("1");
+            man.Dolt(4);
+            Console.WriteLine("2");
+            man.Dolt(5);
+            Console.WriteLine("3");
+            man.Dolt(9);
+            Console.WriteLine("4");
+            man.Dolt(3);
+            Console.WriteLine("5");
+            man.Dolt(20);
+            Console.WriteLine("6");
         }
+
+        private static void Man_dele()
+        {
+            Console.WriteLine("사건발생");
+        }
+
         static int Add(int a,int b)
         {
             Console.WriteLine("Add 메서드");
