178adcc507ba052c1d8a4d4678e867a323a15d5a
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-18 16:07:17 +0900

    Update 테트리스Fin(색, 스페이스, 줄 삭제 추가)

--- 테트리스/Board.cs
+++ 테트리스/Board.cs
@@ -18,7 +18,6 @@ namespace 테트리스
                 return max_x;
             }
         }
-
         internal int MAX_Y
         {
             get
@@ -29,29 +28,88 @@ namespace 테트리스
 
         internal bool CheckMoveEnable(Diagram diagram, int cx, int cy)
         {
-            int dx = diagram.X+cx;
-            int dy = diagram.Y+cy;
+            int dx = diagram.X + cx;
+            int dy = diagram.Y + cy;
             int bn = diagram.BN;
             int tn = diagram.TN;
 
-            for (int x=0;x<4;x++)
+            for (int x = 0; x < 4; x++)
             {
-                for(int y=0;y<4;y++)
-                {   
-                    if(((dy+y)>=20)||((dx+x)<0)||((dx+x)>=10))
+                for (int y = 0; y < 4; y++)
+                {
+                    if (((dy + y) >= 20) || ((dx + x) < 0) || ((dx + x) >= 10))
                     {
-                        if((Diagram.bvalue[bn, tn, y, x] == 1))
+                        if (Diagram.bvalue[bn, tn, y, x] == 1)
                         {
                             return false;
                         }
                     }
-                    if((Diagram.bvalue[bn,tn,y,x]==1) && (bdvals[dy+y,dx+x]==1))
+                    if ((Diagram.bvalue[bn, tn, y, x] == 1) && (bdvals[dy + y, dx + x] != 0))
                     {
                         return false;
                     }
                 }
             }
             return true;
+        }
+
+        internal void Add(Diagram diagram)
+        {
+            int dx = diagram.X;
+            int dy = diagram.Y;
+            int bn = diagram.BN;
+            int tn = diagram.TN;
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    if (((dy + y) >= 20) || ((dx + x) < 0) || ((dx + x) >= 10))
+                    {
+                    }
+                    else if (Diagram.bvalue[bn, tn, y, x] == 1)
+                    {
+                        bdvals[dy + y, dx + x] = bn + 1;
+                    }
+                }
+            }
+
+
+            int cl = dy + 3;
+            for (int cc = 0; cc < 4; cc++)
+            {
+                if (CheckLine(cl) == false)
+                {
+                    cl--;
+                }
+            }
+
+        }
+
+        private bool CheckLine(int cl)
+        {
+            if (cl >= 20)
+            {
+                return false;
+            }
+            for (int i = 0; i < max_x; i++)
+            {
+                if (bdvals[cl, i] == 0)
+                {
+                    return false;
+                }
+            }
+            ClearLine(cl);
+            return true;
+        }
+        private void ClearLine(int cl)
+        {
+            for (int y = cl; y > 0; y--)
+            {
+                for (int x = 0; x < max_x; x++)
+                {
+                    bdvals[y, x] = bdvals[y - 1, x];
+                }
+            }
         }
     }
 }
--- 테트리스/테트리스Form.cs
+++ 테트리스/테트리스Form.cs
@@ -12,6 +12,12 @@ namespace 테트리스
 {
     public partial class 테트리스Form : Form
     {
+        Pen[] pens = new Pen[7]{Pens.Black,Pens.DarkGray,Pens.Cyan,Pens.AliceBlue,
+            Pens.Brown,Pens.Purple,Pens.IndianRed };
+        Brush[] brushes = new Brush[7] {
+            Brushes.AliceBlue,Brushes.Brown,Brushes.Purple,Brushes.IndianRed,
+            Brushes.Black,Brushes.DarkGray,Brushes.Cyan
+        };
         public 테트리스Form()
         {
             InitializeComponent();
@@ -19,16 +25,22 @@ namespace 테트리스
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            switch(e.KeyCode)
+            switch (e.KeyCode)
             {
-                case Keys.Left: Move(-1,0); break;
-                case Keys.Right: Move(1,0); break;
-                case Keys.Up: Turn(); break;
-                case Keys.Down: NextBlock(); break;
+                case Keys.Left: Move(-1, 0); break;
+                case Keys.Right: Move(1, 0); break;
+                case Keys.Up: Turn(); ; break;
+                case Keys.Space: MoveManyDown(); break;
             }
+
         }
 
-        private void NextBlock()
+        private void MoveManyDown()
+        {
+            while (Move(0, 1)) ;
+        }
+
+        private void NextBlcok()
         {
             diagram.SetXY(5, 0);
             Invalidate();
@@ -40,20 +52,35 @@ namespace 테트리스
             Invalidate();
         }
 
-        private void Move(int cx, int cy)
+        private bool Move(int cx, int cy)
         {
+            bool check = false;
             if (board.CheckMoveEnable(diagram, cx, cy))
-            { 
-            diagram.Move(cx, cy);
-            Invalidate();   // 다시그려주세요
+            {
+                diagram.Move(cx, cy);
+                check = true;
             }
+            else
+            {
+                if (cy == 1)
+                {
+                    board.Add(diagram);
+                    diagram.SetXY(5, 0);
+                    if (board.CheckMoveEnable(diagram, 0, 0) == false)
+                    {
+                        tm_down.Enabled = false;
+                        MessageBox.Show("게임 오버");
+                        Close();
+                    }
+                }
+            }
+            Invalidate();
+            return check;
         }
 
         const int step = 30;
         Diagram diagram = new Diagram(5, 0);
         Board board = new Board();
-
-
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
@@ -63,21 +90,20 @@ namespace 테트리스
 
         private void DrawBoard(Graphics graphics)
         {
-            board.bdvals[9, 9] = 1;
-            for (int cy = 0; board.MAX_Y < 4; cy++)
+            for (int cy = 0; cy < board.MAX_Y; cy++)
             {
-                for (int cx = 0; board.MAX_X < 4; cx++)
+                for (int cx = 0; cx < board.MAX_X; cx++)
                 {
-                    if (board.bdvals[cy, cx] == 1)
+                    if (board.bdvals[cy, cx] != 0)
                     {
-                        Rectangle rect = new Rectangle(cx* step, cy * step, step, step);
-                        graphics.DrawRectangle(Pens.Red, rect);
+                        int index = board.bdvals[cy, cx] - 1;
+                        Rectangle rect = new Rectangle(cx * step, cy * step, step, step);
+                        graphics.DrawRectangle(pens[index], rect);
                         rect.X += 2;
                         rect.Y += 2;
                         rect.Width -= 4;
                         rect.Height -= 4;
-                        graphics.FillRectangle(Brushes.BlanchedAlmond, rect);
-
+                        graphics.FillRectangle(brushes[index], rect);
                     }
                 }
             }
@@ -89,28 +115,25 @@ namespace 테트리스
             int x = diagram.X;
             int y = diagram.Y;
             int bn = diagram.BN;
-            for (int cy=0;cy<4;cy++)
+
+            for (int cy = 0; cy < 4; cy++)
             {
-                for(int cx=0;cx<4;cx++)
+                for (int cx = 0; cx < 4; cx++)
                 {
-                    if(Diagram.bvalue[bn,tn,cy,cx]==1)
+                    if (Diagram.bvalue[bn, tn, cy, cx] == 1)
                     {
-                        Rectangle rect = new Rectangle((x + cx)* step, (y + cy)* step, step, step);
-                        graphics.DrawRectangle(Pens.Red, rect);
+                        Rectangle rect = new Rectangle((x + cx) * step, (y + cy) * step, step, step);
+                        graphics.DrawRectangle(pens[bn], rect);
                         rect.X += 2;
                         rect.Y += 2;
                         rect.Width -= 4;
                         rect.Height -= 4;
-                        graphics.FillRectangle(Brushes.BlanchedAlmond, rect);
-
+                        graphics.FillRectangle(brushes[bn], rect);
                     }
                 }
             }
-        }
 
-        private void tm_tm_Tick(object sender, EventArgs e)
-        {
-            lb_time.Text = DateTime.Now.ToShortTimeString();
+
         }
 
         private void tm_down_Tick(object sender, EventArgs e)
@@ -118,13 +141,20 @@ namespace 테트리스
             Move(0, 1);
         }
 
+        private void tm_tm_Tick(object sender, EventArgs e)
+        {
+            lb_time.Text = DateTime.Now.ToShortTimeString();
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             lb_time.Text = DateTime.Now.ToShortTimeString();
 
-            int width = step * board.MAX_X + 18;
-            int height = step + board.MAX_Y + 32;
-            this.Size = new Size(Width, height);
+            int witdth = step * board.MAX_X + 18;
+            int height = step * board.MAX_Y + 40;
+            this.Size = new Size(witdth, height);
+
+
         }
     }
 }
