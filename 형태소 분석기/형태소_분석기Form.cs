9e14c3cbf77520519bba72a49dc787a7c4211f62
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-19 13:48:12 +0900

    Update 형태소_분석기Form.cs

--- 형태소 분석기/형태소_분석기Form.cs
+++ 형태소 분석기/형태소_분석기Form.cs
@@ -44,6 +44,9 @@ namespace App1
                 lbox_org.Items.RemoveAt(0);
             }
         }
+        string dm_file = "dmcnt.txt";   // 파일명 - 형태소 개수
+        string inv_file = "index.txt";  // 역파일 (처음 발견한 형태소 이름)
+        string inv_dir = "inv"; //역파일 폴더명
 
         private void Parsing(string filename)
         {
@@ -59,6 +62,81 @@ namespace App1
             lbox_par.Items.Add(mfname);
             int tot_mcnt = 0;
             Dictionary<string, Morpheme> mor_dic = MorphemeParser.Parse(filename, s,out tot_mcnt);
+
+            SaveDocMoCnt(filename, tot_mcnt);
+            MakeInvFile(filename, mor_dic);
+        }
+
+        private void MakeInvFile(string filename, Dictionary<string, Morpheme> mor_dic)
+        {
+            string ind_filename = string.Format(@"{0}\{1}", tbox_dat.Text, inv_file);
+            if (File.Exists(ind_filename) == false)
+            {
+                FileStream fs = File.Create(ind_filename);
+                fs.Close();
+            }
+            FileStream fs2 = File.OpenRead(ind_filename);
+            StreamReader sr = new StreamReader(fs2);
+            List<string> mors = new List<string>();
+            while (sr.EndOfStream == false)
+            {
+                mors.Add(sr.ReadLine());
+            }
+            sr.Close();
+            fs2.Close();
+
+            foreach(Morpheme mo in mor_dic.Values)
+            {
+                if(mors.Contains(mo.Name)==false)
+                {
+                    mors.Add(mo.Name);
+                }
+                int index = mors.IndexOf(mo.Name);
+                MakeInvFileData(mo,index, filename);
+            }
+
+            FileStream fs3 = File.OpenRead(ind_filename);
+            StreamWriter sw = new StreamWriter(fs3);
+
+            foreach(string name in mors)
+            {
+                sw.WriteLine(name);
+            }
+            sw.Close();
+            fs3.Close();
+
+        }
+
+        private void MakeInvFileData(Morpheme mo, int index, string filename)
+        {
+            string inv_filename = string.Format(@"{0}\{1}\{2}.txt", tbox_dat.Text, inv_dir, index);
+            if(File.Exists(inv_filename)==false)
+            {
+                FileStream fs = File.Create(inv_filename);
+                fs.Close();
+            }
+            FileStream fs2 = File.OpenRead(inv_filename);
+            StreamWriter sw = new StreamWriter(fs2);
+            sw.WriteLine("{0},{1}", filename, index);
+
+            sw.Close();
+            fs2.Close();
+        }
+
+        private void SaveDocMoCnt(string filename, int tot_mcnt)
+        {
+            string dm_filename = string.Format(@"{0}\{1}", tbox_dat.Text, dm_file);
+            if(File.Exists(dm_filename)==false)
+            {
+                FileStream fs = File.Create(dm_filename);
+                fs.Close();
+            }
+            FileStream fs2 = File.Open(dm_filename, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs2);
+            sw.WriteLine("{0},{1}", filename, tot_mcnt);
+            sw.Close();
+            fs2.Close();
+
         }
 
         private string SplitDirFname(string filename, out string dname)
