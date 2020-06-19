665e7fc850358b68b02139876129fb7584bb08ad
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-19 10:31:50 +0900

    add 데이터_입력

---
+++ 데이터_입력Form.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace 어셈블리
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add = new System.Windows.Forms.Button();
+            this.tbox_title = new System.Windows.Forms.TextBox();
+            this.tbox_content = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(37, 398);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "추가";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // tbox_title
+            // 
+            this.tbox_title.Location = new System.Drawing.Point(37, 34);
+            this.tbox_title.Name = "tbox_title";
+            this.tbox_title.Size = new System.Drawing.Size(700, 25);
+            this.tbox_title.TabIndex = 1;
+            // 
+            // tbox_content
+            // 
+            this.tbox_content.Location = new System.Drawing.Point(37, 97);
+            this.tbox_content.Multiline = true;
+            this.tbox_content.Name = "tbox_content";
+            this.tbox_content.Size = new System.Drawing.Size(700, 254);
+            this.tbox_content.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbox_content);
+            this.Controls.Add(this.tbox_title);
+            this.Controls.Add(this.btn_add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox tbox_title;
+        private System.Windows.Forms.TextBox tbox_content;
+    }
+}
+
---
+++ 데이터_입력Form.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 어셈블리
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const string odir = @"D:\EData\Origin";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(Directory.Exists(@"D:\EData")==false)    // @는 
+            {
+                Directory.CreateDirectory(@"D:\EData");
+            }
+            if (Directory.Exists(odir) == false)
+            {
+                Directory.CreateDirectory(odir);
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            string fname = string.Format(@"{0}\{1}.txt", odir, tbox_title.Text);
+            FileStream fs = File.Create(fname);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(tbox_content.Text);
+            sw.Close();
+            fs.Close();
+            tbox_title.Text = tbox_content.Text = "";
+        }
+    }
+}
