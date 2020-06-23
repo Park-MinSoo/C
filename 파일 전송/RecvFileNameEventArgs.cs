1dfbc11cc89d60213afaa458247c69a4a047555f
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-23 11:34:02 +0900

    Create RecvFileNameEventArgs.cs

---
+++ 파일 전송/RecvFileNameEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 파일_수신_서버
+{
+    class RecvFileNameEventArgs : EventArgs
+    {
+        public delegate void RecvFileNameEventHandler(object sender, RecvFileNameEventArgs e);
+        public string FileName { get; private set; }
+        public IPEndPoint RemoteEP { get; private set; }
+
+        public RecvFileNameEventArgs(string fname, IPEndPoint rep)
+        {
+            FileName = fname;
+            RemoteEP = rep;
+        }
+    }
+}
