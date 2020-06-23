44de278d32bb2ff3515066ba17508940f8e1774c
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-23 11:33:45 +0900

    Create FileLengthRecvEventArgs.cs

---
+++ 파일 전송/FileLengthRecvEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 파일_수신_서버
+{
+    public delegate void FileLengthRecvEventHandler(object sender, FileLengthRecvEventArgs e);
+    public class FileLengthRecvEventArgs : EventArgs
+    {
+        public string FileName { get; private set; }
+        public IPEndPoint RemoteEP { get; private set; }
+
+        public int Length { get; private set; }
+
+        public FileLengthRecvEventArgs(string fname, IPEndPoint rep, int length)
+        {
+            FileName = fname;
+            RemoteEP = rep;
+            Length = length;
+        }
+    }
+}
