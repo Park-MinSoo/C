f106bd66c75effc9409367183ff910c15ba6b1ff
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-24 16:45:52 +0900

    create MFC LAB

---
+++ MFC/FirstMFCApp/FirstMFCAppDlg.h
@@ -0,0 +1,39 @@
+﻿
+// FirstMFCAppDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CFirstMFCAppDlg 대화 상자
+class CFirstMFCAppDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CFirstMFCAppDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FIRSTMFCAPP_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonOk();
+	// IDC_EDIT_ONE과 매핑
+	CString one_str;
+	CString two_str;
+};
---
+++ MFC/SecondMFCApp/SecondMFCAppView.cpp
@@ -0,0 +1,111 @@
+﻿
+// SecondMFCAppView.cpp: CSecondMFCAppView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "SecondMFCApp.h"
+#endif
+
+#include "SecondMFCAppDoc.h"
+#include "SecondMFCAppView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSecondMFCAppView
+
+IMPLEMENT_DYNCREATE(CSecondMFCAppView, CView)
+
+BEGIN_MESSAGE_MAP(CSecondMFCAppView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CSecondMFCAppView 생성/소멸
+
+CSecondMFCAppView::CSecondMFCAppView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CSecondMFCAppView::~CSecondMFCAppView()
+{
+}
+
+BOOL CSecondMFCAppView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CSecondMFCAppView 그리기
+
+void CSecondMFCAppView::OnDraw(CDC* pdc/*pDC*/)
+{
+	CSecondMFCAppDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+	RECT rt;
+	rt.left = 10;
+	rt.right = 100;
+	rt.top = 10;
+	rt.bottom = 100;
+	pdc -> Ellipse(&rt);
+}
+
+
+// CSecondMFCAppView 인쇄
+
+BOOL CSecondMFCAppView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CSecondMFCAppView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CSecondMFCAppView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CSecondMFCAppView 진단
+
+#ifdef _DEBUG
+void CSecondMFCAppView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CSecondMFCAppView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CSecondMFCAppDoc* CSecondMFCAppView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSecondMFCAppDoc)));
+	return (CSecondMFCAppDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CSecondMFCAppView 메시지 처리기
