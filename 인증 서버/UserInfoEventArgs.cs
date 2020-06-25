ed8b3b6009d3488a17d90393caca42c8c452322e
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-25 11:15:47 +0900

    Create UserInfoEventArgs.cs

---
+++ 인증 서버/UserInfoEventArgs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 인증_클라이언트
+{
+     delegate void UserInfoEventHandler(object sender, UserInfoEventArgs e);
+    class UserInfoEventArgs:EventArgs
+    {
+        public string ID
+        {
+            get;
+            private set;
+        }
+        public string IPStr
+        {
+            get;
+            private set;
+        }
+
+        public int SPort
+        {
+            get;
+            private set;
+        }
+        public int FPort
+        {
+            get;
+            private set;
+        }
+
+        public UserInfoEventArgs(string id, string ipstr, int sport, int fport)
+        {
+            ID = id;
+            IPStr = ipstr;
+            SPort = sport;
+            FPort = fport;
+        }
+    }
+}
