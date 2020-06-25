0dd15c33e5b96895ea886f706e61c3978c4ed66c
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-25 11:17:41 +0900

    포트 번호 수정

--- 인증 서버/UserInfoCBServer.cs
+++ 인증 서버/UserInfoCBServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -10,6 +11,7 @@ namespace 인증_클라이언트
 {
     class UserInfoCBServer
     {
+        public event UserInfoEventHandler UserInfoEventHandler = null;
         public string IPStr
         {
             get;
@@ -43,7 +45,7 @@ namespace 인증_클라이언트
 
                 //네트워크 인터페이스와 결합
                 IPAddress ipaddr = IPAddress.Parse("192.168.10.69");
-                IPEndPoint iep = new IPEndPoint(ipaddr,10346); 
+                IPEndPoint iep = new IPEndPoint(ipaddr,Port); 
                 // 포트번호는 임의로 지정해 주는 것이며, 1000번대 이하는 보통 중요한 프로그램이 돌아가기때문에 10000번대로 임의로 설정하였다.
                 
                 sock.Bind(iep);
@@ -53,18 +55,57 @@ namespace 인증_클라이언트
 
                 //AcceptLoop - 클라이언트 연결 수락 및 송수신
                 AcceptLoopAsync();
+                return true;
             }
 
             catch
             {
                 return false;
             }
-
         }
 
+        delegate void AcceptDele();
         private void AcceptLoopAsync()
         {
+            AcceptDele dele = AcceptLoop;
+            dele.BeginInvoke(null, null);
+        }
+
+        void AcceptLoop()
+        {
+            Socket dosock;
+            while(true)
+            {
+                dosock = sock.Accept();
+                DoItAsync(dosock);
+            }
+        }
+
+        delegate void DoItDele(Socket dosock);
+        private void DoItAsync(Socket dosock)
+        {
+            DoItDele dele = DoIt;
+            dele.BeginInvoke(dosock, null, null);
+        }
+
+        private void DoIt(Socket dosock)
+        {
+            byte[] packet = new byte[1024];
+            dosock.Receive(packet);
+            MemoryStream ms = new MemoryStream(packet);
+            BinaryReader br = new BinaryReader(ms);
+            string id = br.ReadString();
+            string ip = br.ReadString();
+            int sport = br.ReadInt32();
+            int fport = br.ReadInt32();
+            br.Close();
+            ms.Close();
             
+            if(UserInfoEventHandler != null)
+            {
+                UserInfoEventHandler(this, new UserInfoEventArgs(id, ip, sport, fport));
+            }
+            dosock.Close();
         }
     }
 }
