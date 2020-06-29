364865c40537fc0eead9feedb816f678e26ef680
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-29 09:52:29 +0900

    Create 자기자신의 IP주소 획득하기.cs

---
+++ 자기자신의 IP주소 획득하기.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 자기_자신의_IP주소_획득하기_콘솔_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPHostEntry
+            string hname = Dns.GetHostName();
+            IPHostEntry ihe = Dns.GetHostEntry(hname);
+            foreach(IPAddress ipaddr in ihe.AddressList)
+            {
+                if(ipaddr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine(ipaddr.ToString());
+                }
+            }
+        }
+    }
+}
