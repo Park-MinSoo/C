aee87a4560854ec4fdb71c3dc949a4b3123be5df
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-06-30 09:55:33 +0900

    Create Candidate.cs

---
+++ 웹 수집 로봇/Candidate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 웹_수집_로봇
+{
+    class Candidate
+    {
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        public int Depth
+        {
+            get;
+            set;
+        }
+        public Candidate(string url, int depth)
+        {
+            Url = url;
+            Depth = depth;
+        }
+        public override string ToString()
+        {
+            return Url;
+        }
+    }
+}
