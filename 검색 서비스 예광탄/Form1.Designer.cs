7d3f2294c7596f1024c7529438c756be45b81cc1
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-07-01 14:53:33 +0900

    add Form

---
+++ 검색 서비스 예광탄/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace 검색_서비스_예광탄
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbox_query = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.lv_result = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // tbox_query
+            // 
+            this.tbox_query.Location = new System.Drawing.Point(20, 23);
+            this.tbox_query.Name = "tbox_query";
+            this.tbox_query.Size = new System.Drawing.Size(649, 25);
+            this.tbox_query.TabIndex = 0;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(692, 23);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(97, 24);
+            this.btn_search.TabIndex = 1;
+            this.btn_search.Text = "button1";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // lv_result
+            // 
+            this.lv_result.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lv_result.HideSelection = false;
+            this.lv_result.Location = new System.Drawing.Point(18, 75);
+            this.lv_result.Name = "lv_result";
+            this.lv_result.Size = new System.Drawing.Size(770, 309);
+            this.lv_result.TabIndex = 2;
+            this.lv_result.UseCompatibleStateImageBehavior = false;
+            this.lv_result.View = System.Windows.Forms.View.Details;
+            this.lv_result.SelectedIndexChanged += new System.EventHandler(this.lv_result_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "제목";
+            this.columnHeader1.Width = 319;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Url";
+            this.columnHeader2.Width = 297;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Score(TF_IDF)";
+            this.columnHeader3.Width = 149;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lv_result);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.tbox_query);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbox_query;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.ListView lv_result;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ 검색 서비스 예광탄/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 검색_서비스_예광탄
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string query = tbox_query.Text;
+            List<RankedUrl> rurls = SearchEngine.Search(query);
+            foreach (RankedUrl rurl in rurls)
+            {
+                string[] sitems = new string[] { rurl.Title, rurl.Url, rurl.Score.ToString() };
+                ListViewItem lvi = new ListViewItem(sitems);
+                lv_result.Items.Add(lvi);
+            }
+            Text = tbox_query.Text;
+            tbox_query.Text = "";
+        }
+
+        private void lv_result_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lv_result.SelectedItems.Count <=0)
+            {
+                return;
+            }
+            ListViewItem lvi = lv_result.SelectedItems[0];
+            string url = lvi.SubItems[1].Text;
+            MWebBrowser mwb = new MWebBrowser(url);
+            mwb.Show();
+        }
+    }
+
+}
