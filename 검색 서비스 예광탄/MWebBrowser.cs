f419ac978e4843fea0ebe9893e93bb444cff8d72
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-07-01 14:54:04 +0900

    add MWebBrowser Form

---
+++ 검색 서비스 예광탄/MWebBrowser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 검색_서비스_예광탄
+{
+    public partial class MWebBrowser : Form
+    {
+        public MWebBrowser(string url)
+        {
+            InitializeComponent();
+            webBrowser1.Navigate(url);
+        }
+    }
+}
