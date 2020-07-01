e3bcf083a6be6acedae9e0bba7d23184115a9508
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-07-01 14:54:16 +0900

    Create RankedUrl.cs

---
+++ 검색 서비스 예광탄/RankedUrl.cs
@@ -0,0 +1,42 @@
+﻿namespace 검색_서비스_예광탄
+{
+    public class RankedUrl
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public string Url
+        {
+            get;
+            private set;
+        }
+
+        public double Score
+        {
+            get;
+            private set;
+        }
+
+        public string Content
+        {
+            get;
+            private set;
+        }
+        public RankedUrl(string title, string url, double score, string content)
+        {
+            Title = title;
+            Url = url;
+            Score = score;
+            Content = content;
+        }
+
+        public static RankedUrl Merge(RankedUrl r1, RankedUrl r2)
+        {
+            r1.Score += r2.Score;
+            return r1;
+        }
+    }
+}
