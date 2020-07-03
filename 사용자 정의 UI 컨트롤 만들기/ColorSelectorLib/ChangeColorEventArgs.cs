868f13ca1a65f1b0a42951c2d880b1038d4f3014
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-07-03 10:26:17 +0900

    add ChangeColorEventArgs

---
+++ 사용자 정의 UI 컨트롤 만들기/ColorSelectorLib/ChangeColorEventArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorSelectorLib
+{
+    public delegate void ChangeColorEventHandler(object sender, ChangeColorEventArgs e);
+    public class ChangeColorEventArgs:EventArgs
+    {
+        public int Red
+        {
+            get;
+            private set;
+        }
+        public int Green
+        {
+            get;
+            private set;
+        }
+        public int Blue
+        {
+            get;
+            private set;
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return Color.FromArgb(Red, Green, Blue);
+            }
+        }
+        public ChangeColorEventArgs(int r, int g, int b)
+        {
+            Red = r;
+            Green = g;
+            Blue = b;
+        }
+    }
+}
